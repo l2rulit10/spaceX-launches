3b3bc3b506fb747fbc5e83fc1bca110b230177e6
Zhaksylyk <ualievzakylyk@gmail.com>
2019-03-30 23:37:16 +0600

    Finished React

---
+++ public/precache-manifest.616c65dbc667608bd6ecf231ca1313c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "631c1f38680c91d4893b",
+    "url": "/static/css/main.9ecfcda9.chunk.css"
+  },
+  {
+    "revision": "631c1f38680c91d4893b",
+    "url": "/static/js/main.98d92c48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ce4759d56a492eb68a3",
+    "url": "/static/js/2.da6cff33.chunk.js"
+  },
+  {
+    "revision": "2d87f0a67af54c3f5feafe2e38a93512",
+    "url": "/index.html"
+  }
+];
